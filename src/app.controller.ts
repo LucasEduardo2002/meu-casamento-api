60870863ed58fce68a330686448e71d8449a532d
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-07-03 11:29:06 -0300

    feat: add HEAD route to AppController to support health checks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Head, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Head()
+  @HttpCode(HttpStatus.OK)
+  getHelloHead() {
+    return;
   }
 }
